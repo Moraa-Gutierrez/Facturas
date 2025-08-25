600ace3d0c8d03184fbdf757ee2402779c97a22e
CiroBlanco <blancociro43@Gmail.com>
2025-08-25 17:18:33 -0300

    Clase 25/8

--- Facturas/Factura.cs
+++ Facturas/Factura.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+
++using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +18,30 @@ namespace Facturas
         public static Factura TraerFactura(int Id_Factura)
         {
             Factura factura = new Factura();
+        }
+        public void InsertarEnBD()
+        {
+            string consulta = "INSERT INTO factura (nombre, relleno, precio) VALUES (@nombre, @relleno, @precio)";
+             List<SqliteParameter> parametros = new List<SqliteParameter>();
+            parametros.Add(new SqliteParameter("@Nombre",Nombre));
+            parametros.Add(new SqliteParameter("@Relleno", Relleno));
+            parametros.Add(new SqliteParameter("@Precio", Precio));
+            GestorConexion.Instancia.ModificarDB(consulta,parametros);
+        }
+        public void ModificarEnBD()
+        {
+            string consulta = "UPDATE factura SET @Nombre = Nombre, @Relleno = Relleno, @Precio = Precio WHERE Id_Factura = @Id_Factura";
+            List<SqliteParameter> parametros = new List<SqliteParameter>();
+            parametros.Add(new SqliteParameter("@Nombre", Nombre));
+            parametros.Add(new SqliteParameter("@Relleno", Relleno));
+            parametros.Add(new SqliteParameter("@Precio", Precio));
+            parametros.Add(new SqliteParameter("@Id_Factura", Id_Factura));
+            GestorConexion.Instancia.ModificarDB(consulta, parametros);
+        }
+        public void EliminarEnBD()
+        {   
+            string consulta = "DELETE FROM factura WHERE Id_Factura = @Id_Factura";
+            GestorConexion.Instancia.ModificarDB(consulta);
         }
     }
 }
